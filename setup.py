b2dd07cc3b7da8899f8ee49a76a29ddac5d10a6e
The-Anh Vu-Le <vltanh@illinois.edu>
2023-03-31 20:41:56 -0500

    setup scripts

--- setup.py
+++ setup.py
@@ -4,18 +4,21 @@ from setuptools import setup, find_namespace_packages
 
 FRAMEWORK_NAME = 'fedml'
 
-setup(name=f'colink-unifed-{FRAMEWORK_NAME}',
-      version='0.0',
-      packages=find_namespace_packages(
-          'src', exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
-      package_dir={'': 'src'},
-      install_requires=[
-          'colink >= 0.3.0',
-          "flbenchmark",
-      ],
-      entry_points={
-          'console_scripts': [
-              f'unifed-{FRAMEWORK_NAME} = unifed.frameworks.{FRAMEWORK_NAME}:run_protocol',
-          ] + ([] if FRAMEWORK_NAME != 'example' else ["unifed-example-workload = unifed.frameworks.example:simulate_workload"]),
-      }
-      )
+setup(
+    name=f'colink-unifed-{FRAMEWORK_NAME}',
+    version='0.0',
+    packages=find_namespace_packages(
+        'src', exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    package_dir={'': 'src'},
+    install_requires=[
+        'colink >= 0.3.0',
+        "flbenchmark",
+    ],
+    entry_points={
+        'console_scripts': [
+            f'unifed-{FRAMEWORK_NAME} = unifed.frameworks.{FRAMEWORK_NAME}:run_protocol',
+        ] + (
+            [f'unifed-{FRAMEWORK_NAME}-workload = unifed.frameworks.{FRAMEWORK_NAME}:simulate_workload']
+        ),
+    }
+)
